072856ce4c84f17c87c5c08f2a26d24bd7f323cf
Muhammad Abdullah Azzam <79858774+MuhammadAbdullahAzzam@users.noreply.github.com>
2022-05-19 00:48:59 +0700

    first commit

--- src/main/java/PAE/KTP1_018/DataController.java
+++ src/main/java/PAE/KTP1_018/DataController.java
@@ -4,19 +4,45 @@
  */
 package PAE.KTP1_018;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
- * @author R.O.G
+ * @author ASUS
  */
 @Controller
-
 public class DataController {
     
-@RequestMapping("/data")
-    public String getDataKTP(){
+    DataJpaController datactrl = new DataJpaController();
+    List<Data> newdata = new ArrayList<>();
+    
+    @RequestMapping("/main")
+    public String getMain(){
+        return "menu";
+    }
+    
+    @RequestMapping("/data")
+    //@ResponseBody
+    public String getDataKTP(Model model){
+        int record = datactrl.getDataCount();
+        String result="";
+        try{
+            newdata = datactrl.findDataEntities().subList(0, record);
+        }
+        catch (Exception e){result = e.getMessage();}
+        model.addAttribute("goData", newdata);
+        model.addAttribute("record", record);
+        
         return "database";
+    }
+    
+    @RequestMapping("/edit")
+    public String doEdit(){
+        return "editktp";
     }
 }
---
+++ src/main/java/PAE/KTP1_018/coba/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package PAE.KTP1_018.coba.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
