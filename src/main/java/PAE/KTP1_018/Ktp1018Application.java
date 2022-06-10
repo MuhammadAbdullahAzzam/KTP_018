0f80a3e141ed6fd552575c7534904fd53519fece
Muhammad Abdullah Azzam <79858774+MuhammadAbdullahAzzam@users.noreply.github.com>
2022-06-10 12:41:06 +0700

    first commit

--- src/main/java/PAE/KTP1_018/Ktp1018Application.java
+++ src/main/java/PAE/KTP1_018/Ktp1018Application.java
@@ -2,12 +2,18 @@ package PAE.KTP1_018;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Ktp1018Application {
+public class Ktp1018Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ktp1018Application.class, args);
 	}
-
+        @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Ktp1018Application.class);
+    }
+    
 }
